5c1544c1f8de878e58bdd0c7b63ae14718f99286
Chris Favela <cfavela21@yahoo.com>
2013-07-08 12:31:37 -0700

    added input files

---
+++ dxframework/dxframework/InputManager.h
@@ -0,0 +1,35 @@
+#pragma once
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+
+class InputManager
+{
+public:
+	InputManager(void);
+	~InputManager(void);
+
+	bool init(HINSTANCE hInst, HWND wndHandle);
+
+	void getInput();
+	//mouse functions
+	bool isButtonDown(int button);
+	int getMouseMovingX();
+	int getMouseMovingY();
+	//keyboard functions
+	bool keyDown(DWORD key);
+	bool keyUp(DWORD key);
+	bool keyPress(DWORD key);
+
+private:
+
+	LPDIRECTINPUT8        dInput;			// the direct input object
+	LPDIRECTINPUTDEVICE8  mouseDevice;		// the direct input device for the mouse
+	LPDIRECTINPUTDEVICE8 keyboardDevice;	// the direct input device for the keyboard
+
+	DIMOUSESTATE mouseState;			// The current state of the mouse device
+
+	
+	UCHAR keyState[256];				//state of the keys
+	UCHAR keyPressState[256];			//used for the keyPressed function
+
+};
