3dace102c5a235e4ea49709be4c3303088a0b5b4
LukeCossel <daringeagle@sbcglobal.net>
2013-07-15 13:03:34 -0700

    remove test comment

--- dxframework/dxframework/DirectXFramework.h
+++ dxframework/dxframework/DirectXFramework.h
@@ -19,7 +19,7 @@
 //////////////////////////////////////////////////////////////////////////
 #include <d3d9.h>
 #include <d3dx9.h>
-// test
+
 #pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
